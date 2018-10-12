552bf0d15c0138ac6e9391cfce4dcffee9c65789
Ester Pratt <esterpratt@gmail.com>
2018-10-12 14:15:41 +0300

    img update2

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function renderProjs(projs) {
                             <i class="fa fa-plus fa-3x"></i>
                         </div>
                         </div>
-                        <img class="img-fluid" src="img/portfolio/${proj.id}.png" alt="">
+                        <img class="img-fluid" src="img/portfolio/${proj.img}" alt="">
                     </a>
                     <div class="portfolio-caption">
                         <h4>${proj.name}</h4>
