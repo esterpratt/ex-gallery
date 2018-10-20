43dfedd89534394402a9fbb32318086afad8c970
Ester Pratt <esterpratt@gmail.com>
2018-10-20 17:38:13 +0300

    imgs and proj added

---
+++ projs/design2LivePage/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function toggleMenu() {
+    document.querySelector('.main-nav').classList.toggle('open');
+}
+
+function toggleModal() {
+    document.querySelector('.modal-container').classList.toggle('open');
+}
